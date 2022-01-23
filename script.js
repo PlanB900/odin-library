691422cb055a7b2f4f1d7b5bcf1905aae26a2f5b
PlanB900 <rythodir@gmail.com>
2022-01-23 00:57:02 -0700

    Refactor some logic, create modal IIFE

--- script.js
+++ script.js
@@ -1,78 +1,72 @@
-/*
-Multiple fields for user to enter information about book. When 'add' button is pressed,
-text content from each is taken and sent as parameters into Book() constructor.
-*/
-
 let modal = document.getElementById("addBookForm");
 let modalBtn = document.getElementById("modalBtn");
 let closeBtn = document.getElementById("closeBtn");
 
 let addBook = document.getElementById("addBook");
-let titleField = document.getElementById("titleField");
-let authorField = document.getElementById("authorField");
-let pagesField = document.getElementById("pagesField");
+let bookFields = Array.from(document.getElementsByClassName('bookField'));
 let markReadBtn = document.getElementById("markReadBtn");
-
-let errorFieldTitle = document.getElementById("errorFieldTitle");
-let errorFieldAuthor = document.getElementById("errorFieldAuthor");
-let errorFieldPages = document.getElementById("errorFieldPages");
 
 let bookContainer = document.getElementById("bookContainer");
 let library = [];
 
+const Modal = (() => {
 
-
-//Book add listener
-modalBtn.addEventListener('click',openModal);
-
-//Function to open screen
-function openModal(){
-   modal.style.display = 'block';
-   closeBtn.addEventListener('click',closeModal, {once: true});
-   window.addEventListener('click',clickOutside);
-}
-
-//Function to close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
-
-//Function to close modal if outside click
-function clickOutside(e){
-    if(e.target == modal){
-        closeModal();
+    let closeModal = () => {
+        modal.style.display = 'none';
     }
-}
+
+    let clickOutside = (e) => {
+        if(e.target == modal){
+            closeModal();
+        }
+    }
+
+    let openModal = () => {
+        modal.style.display = 'block';
+        closeBtn.addEventListener('click',closeModal, {once: true});
+        window.addEventListener('click',clickOutside);
+    }
+
+    modalBtn.addEventListener('click',openModal);
+    markReadBtn.addEventListener('click',markReadBtnFunctionality)
+
+})()
 
 //Checks if field input types are correct
 function fieldCheck () {
-    if(titleField.value == ""){
-        errorFieldTitle.style.display = "flex";
-    }else{errorFieldTitle.style.display = "none"};
 
-    if(authorField.value == ""){
-        errorFieldAuthor.style.display = "flex";
-    }else{errorFieldAuthor.style.display = "none"};
+    function createErrorField(bookField, type){
+        let errorField = document.createElement('div')
+        errorField.className = "errorField"
+
+        type === "notPages" ? errorField.textContent = `*Required` : 
+        errorField.textContent = `Please enter a page number.`
+
+        bookField.parentNode.lastChild.remove()
+        bookField.parentNode.appendChild(errorField)
+    }
     
-    if(pagesField.value == "" || isNaN(pagesField.value)){
-        errorFieldPages.style.display = "flex";
-    }else{errorFieldPages.style.display = "none"};
+    let errorCounter = 0
+    bookFields.forEach((bookField, index) => {
+        if (index == 2 && isNaN(bookField.value)){
+            createErrorField(bookField, "Pages")
+            errorCounter++
+        } else if (bookField.value == ""){
+            createErrorField(bookField, "notPages")
+            errorCounter++
+        }
+    })
 
-    if(titleField.value !== "" && authorField.value !== "" && isNaN(pagesField.value) !== true &&
-    pagesField.value !== ""){
-        pagesField.className = "";
-        let book =  new Book(titleField.value,authorField.value,pagesField.value,markReadBtn.textContent);
+    if(errorCounter === 0){
+        let book =  new Book(bookFields[0].value,bookFields[1].value,bookFields[2].value,markReadBtn.textContent);
         library.push(book);
-        titleField.value = "";
-        authorField.value = "";
-        pagesField.value = "";
+        bookFields.forEach(bookField => {bookField.value = ""})
         createBookCard(book)
-        // createCard();
     };
 }
 
 //Listen for button click to add book to library
-addBook.addEventListener('click',fieldCheck);
+addBook.addEventListener('click', fieldCheck);
 
 //Book constructor
 class Book {
@@ -93,7 +87,7 @@ class Book {
     }
 }
 
-//Create book card template
+//Create book card template, used by createBookCard
 function createBookCardTemplate() {
     let bookCard = document.createElement('div');
     let removeBtn = document.createElement('button');
@@ -106,6 +100,9 @@ function createBookCardTemplate() {
     markReadCardBtn.className = "markReadCard";
     bookCardInfo.className = "bookCardInfo";
 
+    removeBtn.addEventListener('click', removeBtnFunctionality, {once: true})
+    markReadCardBtn.addEventListener('click', markReadBtnFunctionality)
+
     bookContainer.appendChild(bookCard);
     bookCard.appendChild(removeBtn);
     bookCard.appendChild(markReadCardBtn);
@@ -114,12 +111,45 @@ function createBookCardTemplate() {
     return {bookCardInfo, markReadCardBtn}
 }
 
+//Mark read button functionality
+function markReadBtnFunctionality(e){
+    if(e.target.classList.contains("markReadCard") || e.target.id == "markReadBtn"){
+        if(e.target.textContent == "Unread"){
+            e.target.textContent = "Read";
+            e.target.classList.remove("Unread")
+            e.target.classList.add("Read");
+        } else {
+            e.target.textContent = "Unread";
+            e.target.classList.remove("Read")
+            e.target.classList.add("Unread");
+        };
+    };
+}
+
+//Remove button functionality
+function removeBtnFunctionality(e){
+    if(e.target.className == "removeBtn"){
+        //Matches info in bookCard with each book in the library, if they match
+        //it is removed from the library
+        library.forEach(book=>{
+            if(book.title == e.target.nextElementSibling.nextElementSibling.firstChild.textContent &&
+                book.author == e.target.nextElementSibling.nextElementSibling.firstChild.nextElementSibling.textContent){
+                    library.splice(library.indexOf(book),1);
+                };
+        });
+        //It is then removed from the bookContainer
+        e.target.parentNode.remove();
+    };
+}
+
 //creates a card for a given book
 function createBookCard(book){
 
     let template = createBookCardTemplate()
     template.markReadCardBtn.textContent = book.isRead
-    book.isRead == "Read" ? template.markReadCardBtn.classList.add("Read") : template.markReadCardBtn.classList.add("Unread")
+
+    book.isRead == "Read" ? template.markReadCardBtn.classList.add("Read") : 
+    template.markReadCardBtn.classList.add("Unread")
 
     for (let key in book){
         if(key !== "isRead"){
@@ -136,36 +166,3 @@ function createBookCard(book){
         }
     };
 }
-
-//Listen for remove button click
-document.addEventListener('click',(e)=>{
-    if(e.target.className == "removeBtn"){
-
-        //Matches info in bookCard with each book in the library, if they match
-        //it is removed from the library
-        library.forEach(book=>{
-            if(book.title == e.target.nextElementSibling.nextElementSibling.firstChild.textContent &&
-                book.author == e.target.nextElementSibling.nextElementSibling.firstChild.nextElementSibling.textContent){
-                    library.splice(library.indexOf(book),1);
-                };
-        });
-        
-        //It is then removed from the bookContainer
-        e.target.parentNode.remove();
-    };
-
-    //For mark read button on cards and in add card screen
-    if(e.target.classList.contains("markReadCard") || e.target.id == "markReadBtn"){
-        if(e.target.textContent == "Unread"){
-            e.target.textContent = "Read";
-            e.target.classList.remove("Unread")
-            e.target.classList.add("Read");
-        }else if(e.target.textContent == "Read"){
-            e.target.textContent = "Unread";
-            e.target.classList.remove("Read")
-            e.target.classList.add("Unread");
-        };
-    };
-    
-});
-
